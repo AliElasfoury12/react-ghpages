b81c277d2fd806ccacf56c87e01f2c36fc02b733
Ali Elasfoury <alielasfoury12@gmail.com>
2024-12-10 22:23:53 +0200

    base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploy-github/",
+  base: "https://alielasfoury12.github.io/react-ghpages/",
   plugins: [react()],
 })
